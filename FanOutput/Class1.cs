894cbf1e5423ac077823405a0a29cb933b66afa1
younzable <youn0079@edu.ucl.dk>
2020-10-05 11:20:46 +0200

    asd

---
+++ FanOutput/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace Opgave1
+{
+    public class Class1
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Temp
+        {
+            get;
+            set;
+        }
+        public int Fugt { get; set; }
+    }
+}
