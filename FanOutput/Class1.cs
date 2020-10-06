0e6a06c46d69110b662c1fd58e7c1e715aefe001
younzable <youn0079@edu.ucl.dk>
2020-10-06 12:04:37 +0200

    added code and started implementing the exeptions

--- FanOutput/Class1.cs
+++ FanOutput/Class1.cs
@@ -1,14 +1,29 @@
-﻿namespace Opgave1
+﻿using System;
+
+namespace Opgave1
 {
     public class Class1
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
+
         public int Temp
         {
             get;
-            set;
+            set
+            {
+                if (value <15|| value>25)
+                {
+                    throw new NotFiniteNumberException();
+                }
+                else
+                {
+                    value = Temp;
+                }
+            }
         }
+
         public int Fugt { get; set; }
     }
 }
